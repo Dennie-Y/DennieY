59ea97fb3dc2e3dece4b4ce349d646009a8941d2
Dennie-Y <dennieyuen.dev@gmail.com>
2025-04-15 22:20:58 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,14 +6,13 @@ const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
     link.addEventListener("click", function (e) {
-        e.preventDefault();
         const href = link.getAttribute("href");
 
-        if (href === "#")
-            window.scrollTo({
-                top: 0,
-                behavior: "smooth",
-            });
+        if (!href.startsWith("#") || href === "#") {
+            return;
+        }
+
+        e.preventDefault();
 
         if (href !== "#" && href.startsWith("#")) {
             const sectionEl = document.querySelector(href);
